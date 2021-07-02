111059633321301021498778dec3982235d55a34
XXGGG <534464179@qq.com>
2021-07-03 00:02:03 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <returnKey v-if="this.$route.path != '/'" />
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-  </div>
+  <cube/>
 </template>
 
-<script>
-import returnKey from '@/components/returnKey.vue'; 
-export default ({
-  name:'App',
-  components: {
-    returnKey,
-  },
-  created(){
-  },
-  methods:{
-    
+<script lang="ts">
+import { defineComponent } from 'vue'
+import cube from './components/cube.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: { 
+    cube
   }
 })
 </script>
+
 <style>
-/* #app::-webkit-scrollbar{
-  display: none;
-} */
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgb(236, 236, 236);
+}
 </style>
---
+++ src/components/cube.vue
@@ -0,0 +1,58 @@
+<template>
+  <CubeDark class="cube"></CubeDark>
+  <CubeLight class="cube"></CubeLight>
+  <CubeRed class="cube"></CubeRed>
+  <CubeGreen class="cube"></CubeGreen>
+  <CubeBlue class="cube"></CubeBlue>
+  <CubeOrange class="cube"></CubeOrange>
+  <CubeYellow class="cube"></CubeYellow>
+  <CubeTeal class="cube"></CubeTeal>
+  <CubePurple class="cube"></CubePurple>
+  <CubePink class="cube"></CubePink>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import CubeDark from './cube/CubeDark.vue'
+import CubeLight from './cube/CubeLight.vue'
+import CubeRed from './cube/CubeRed.vue'
+import CubeGreen from './cube/CubeGreen.vue'
+import CubeBlue from './cube/CubeBlue.vue'
+import CubeOrange from './cube/CubeOrange.vue'
+import CubeYellow from './cube/CubeYellow.vue'
+import CubeTeal from './cube/CubeTeal.vue'
+import CubePurple from './cube/CubePurple.vue'
+import CubePink from './cube/CubePink.vue'
+
+
+export default defineComponent({
+  name: 'cube',
+  components:{
+    CubeDark,
+    CubeLight,
+    CubeRed,
+    CubeGreen,
+    CubeBlue,
+    CubeOrange,
+    CubeYellow,
+    CubeTeal,
+    CubePurple,
+    CubePink,
+  },
+  props: {
+    
+  },
+  setup: () => {
+    const count = ref(0)
+    return { count }
+  }
+})
+</script>
+
+<style scoped>
+.cube{
+  width: 10%;
+  height: auto;
+  cursor: pointer;
+}
+</style>
