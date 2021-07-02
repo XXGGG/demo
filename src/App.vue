ac4f65170d95613922dba404ef4ffb4dfa9a318d
XXGGG <534464179@qq.com>
2021-07-03 00:25:57 +0800

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <cube/>
+  <!-- <HelloWorld/> -->
+  <CubeDark class="cube"></CubeDark>
+  <CubeLight class="cube"></CubeLight>
+  <CubeRed class="cube"></CubeRed>
+  <CubeGreen class="cube"></CubeGreen>
+  <CubeBlue class="cube"></CubeBlue>
+  <CubeOrange class="cube"></CubeOrange>
+  <CubeYellow class="cube"></CubeYellow>
+  <CubeTeal class="cube"></CubeTeal>
+  <CubePurple class="cube"></CubePurple>
+  <CubePink class="cube"></CubePink>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import cube from './components/cube.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import CubeDark from './components/cube/CubeDark.vue'
+import CubeLight from './components/cube/CubeLight.vue'
+import CubeRed from './components/cube/CubeRed.vue'
+import CubeGreen from './components/cube/CubeGreen.vue'
+import CubeBlue from './components/cube/CubeBlue.vue'
+import CubeOrange from './components/cube/CubeOrange.vue'
+import CubeYellow from './components/cube/CubeYellow.vue'
+import CubeTeal from './components/cube/CubeTeal.vue'
+import CubePurple from './components/cube/CubePurple.vue'
+import CubePink from './components/cube/CubePink.vue'
 export default defineComponent({
   name: 'App',
   components: { 
-    cube
+    // HelloWorld,
+    CubeDark,
+    CubeLight,
+    CubeRed,
+    CubeGreen,
+    CubeBlue,
+    CubeOrange,
+    CubeYellow,
+    CubeTeal,
+    CubePurple,
+    CubePink,
   }
 })
 </script>
